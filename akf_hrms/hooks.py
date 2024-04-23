1fa6d32e42f6b2703495b4f13dc97d38bc16b70e
“nabeelsocailuse” <nabeel.social.use@gmail.com>
2024-04-23 13:26:11 +0000

    Al khidmat foundation hrms app pushed to github repo.

--- akf_hrms/hooks.py
+++ akf_hrms/hooks.py
@@ -122,13 +122,13 @@ app_license = "mit"
 # ---------------
 # Hook on document methods and events
 
-# doc_events = {
-	# "*": {
+doc_events = {
+	"*": {
 # 		"on_update": "method",
 # 		"on_cancel": "method",
 # 		"on_trash": "method"
-	# }
-# }
+	}
+}
 
 # Scheduled Tasks
 # ---------------
