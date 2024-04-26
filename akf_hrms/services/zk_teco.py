08d52d981c954f4e4887ffccf9d9ef0d58e00c8e
nabeelsocailuse <nabeel.social.use@gmail.com>
2024-04-26 10:10:47 +0000

    zk teco file changes.

--- akf_hrms/services/zk_teco.py
+++ akf_hrms/services/zk_teco.py
@@ -6,10 +6,10 @@ def get_attendance_zk_teco():
     zk = ZK('103.27.22.130', port=4370, timeout=500, password=0, force_udp=True, ommit_ping=True)
     conn = None
     try:
-        print(zk)
+        # print(zk)
         # connect to device
         conn = zk.connect()
-        print(conn)
+        # print(conn)
         users = conn.get_users()
         print(users)
     except Exception as e:
