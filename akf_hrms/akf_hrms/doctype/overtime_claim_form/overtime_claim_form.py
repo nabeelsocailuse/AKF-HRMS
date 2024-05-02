f3ff57f589b6957d55bc0d08d95a9d580f403187
Nabeel Saleem <167871133+nabeelsocailuse@users.noreply.github.com>
2024-05-02 18:53:50 +0500

    Akf hrms dev (#25)
    * doc events folders init in akf_hrms app.
    
    * (custom fields, doctypes) are added.
    
    * fixtures comment in hooks.py
    
    * removed fixtures from this branch.
    
    * removed fixtures from this branch.
    
    * zk teco file changes.
    
    * set query added to employee_id linked with overtime_allowed field in employee doctype.
    
    * { left brack error in refresh function of overtime_claim_form.js fixed.
    
    * new custom fields added in two doctypes Employee Grievance | Employee Boarding Activity.
    
    * autoname naming series added in OCF doctype.
    
    * branch json added.
    
    * branch abbrievation field added.
    
    * In Out Timing Report(Ittehad)
    
    * In Out Timing Report added to app AKF HRMS.
    
    * Policy (Excel Lab) move to akf-hrms apps. working
    
    * Shehbaz on dev server added, customize doctypes.
    
    * custom folder move from doctype to main akf-hrms module folder.

--- akf_hrms/akf_hrms/doctype/overtime_claim_form/overtime_claim_form.py
+++ akf_hrms/akf_hrms/doctype/overtime_claim_form/overtime_claim_form.py
@@ -6,7 +6,6 @@ from frappe.model.document import Document
 
 
 class OvertimeClaimForm(Document):
-    # pass
 	def autoname(self):
 		from frappe.model.naming import make_autoname
 		self.name = make_autoname(self.naming_series%{
