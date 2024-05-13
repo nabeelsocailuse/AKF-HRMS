13df1cf28f76bd6d0a3a6c8a5cec47bedbb33626
mubarrim-iqbal <mubarrim@gmail.com>
2024-05-13 18:22:05 +0500

    changes in loan Application from Sir Shehbaz

--- akf_hrms/public/js/loan_application.js
+++ akf_hrms/public/js/loan_application.js
@@ -1,23 +1,30 @@
 frappe.ui.form.on("Loan Application", {
   loan_product: function (frm) {
-    if (frm.doc.loan_product == "Advance Salary") {
+    if (
+      frm.doc.loan_product == "Advance Salary" ||
+      frm.doc.loan_product == "Advance Salary - Endowment Fund Trust" ||
+      frm.doc.loan_product ==
+        "Advance Salary - Alkhidmat Islamic Microfinance" ||
+      frm.doc.loan_product == "Advance Salary - Alkhidmat Health Foundation" ||
+      frm.doc.loan_product == "Advance Salary - Alfalah Scholarship"
+    ) {
       frm.set_value("repayment_method", "Repay Fixed Amount per Period");
       frappe.call({
-        method: "frappe.client.get_value",
+        method: "frappe.client.get_list",
         args: {
           doctype: "Salary Structure Assignment",
+          fields: ["base"],
           filters: {
             employee: frm.doc.applicant,
             docstatus: 1,
             from_date: ["<", new Date()],
           },
-          // or_filters: [["from_date", "greater than", "2024-05-13"]],
-          fieldname: ["base"],
+          order_by: "from_date DESC",
+          limit_page_length: 1,
         },
         callback: function (r) {
-          // console.log(r.message[0]);
-          if (r.message[0]) {
-            frm.set_value("custom_maximum_allowed_loan", r.message.base / 2);
+          if (r.message) {
+            frm.set_value("custom_maximum_allowed_loan", r.message[0].base / 2);
             frm.set_value("loan_amount", frm.doc.custom_maximum_allowed_loan);
             frm.set_value(
               "repayment_amount",
@@ -32,7 +39,6 @@ frappe.ui.form.on("Loan Application", {
               )
             );
           }
-          // frappe.msgprint(__("No Salary Structure Assignment found"));
         },
       });
     } else {
@@ -40,7 +46,14 @@ frappe.ui.form.on("Loan Application", {
     }
   },
   loan_amount: function (frm) {
-    if (frm.doc.loan_product == "Advance Salary") {
+    if (
+      frm.doc.loan_product == "Advance Salary" ||
+      frm.doc.loan_product == "Advance Salary - Endowment Fund Trust" ||
+      frm.doc.loan_product ==
+        "Advance Salary - Alkhidmat Islamic Microfinance" ||
+      frm.doc.loan_product == "Advance Salary - Alkhidmat Health Foundation" ||
+      frm.doc.loan_product == "Advance Salary - Alfalah Scholarship"
+    ) {
       frm.set_value("repayment_method", "Repay Fixed Amount per Period");
       frm.set_value("repayment_amount", frm.doc.loan_amount);
       frm.set_df_property("repayment_method", "read_only", 1);
