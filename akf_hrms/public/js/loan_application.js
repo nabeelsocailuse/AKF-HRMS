7b9b089ec5e82bd2e43d78c864f31d9600593a1b
nabeelsocailuse <nabeel.social.use@gmail.com>
2024-05-13 06:56:13 +0000

    Loan Application, Employee (Onboarding, Seperation) issue fixed.

--- akf_hrms/public/js/loan_application.js
+++ akf_hrms/public/js/loan_application.js
@@ -11,20 +11,20 @@ frappe.ui.form.on("Loan Application", {
         },
         callback: function (r) {
           if (r.message) {
-            frm.set_value("maximum_allowed_loan", r.message.base / 2);
-            frm.set_value("loan_amount", frm.doc.maximum_allowed_loan);
+            frm.set_value("custom_maximum_allowed_loan", r.message.base / 2);
+            frm.set_value("loan_amount", frm.doc.custom_maximum_allowed_loan);
             frm.set_value(
               "repayment_amount",
-              frm.doc.maximum_allowed_loan
+              frm.doc.custom_custom_maximum_allowed_loan
             );
-            frm.set_df_property("maximum_allowed_loan", "read_only", 1);
+            frm.set_df_property("custom_custom_maximum_allowed_loan", "read_only", 1);
           } else {
             frappe.msgprint(__("No Salary Structure Assignment found"));
           }
         },
       });
     } else {
-      frm.set_df_property("maximum_allowed_loan", "read_only", 0);
+      frm.set_df_property("custom_custom_maximum_allowed_loan", "read_only", 0);
     }
   },
   loan_amount: function (frm) {
@@ -34,15 +34,15 @@ frappe.ui.form.on("Loan Application", {
       frm.set_df_property("repayment_method", "read_only", 1);
       frm.set_df_property("repayment_amount", "read_only", 1);
 
-      if (frm.doc.loan_amount > frm.doc.maximum_allowed_loan) {
+      if (frm.doc.loan_amount > frm.doc.custom_custom_maximum_allowed_loan) {
         frappe.msgprint(
           __(
             "You cannot apply Loan more than " +
-              frm.doc.maximum_allowed_loan
+              frm.doc.custom_maximum_allowed_loan
           )
         );
 
-        frm.set_value("loan_amount", frm.doc.maximum_allowed_loan);
+        frm.set_value("loan_amount", frm.doc.custom_maximum_allowed_loan);
         return;
       }
     } else {
