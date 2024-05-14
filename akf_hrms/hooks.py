6b13cbf88b4e684c4df22879b9408ba41f6857b1
nabeelsocailuse <nabeel.social.use@gmail.com>
2024-05-14 08:08:51 +0000

    fixtures deleted, hooks modified, after-install procedure stopped.

--- akf_hrms/hooks.py
+++ akf_hrms/hooks.py
@@ -88,7 +88,7 @@ doctype_js = {
 # ------------
 
 # before_install = "akf_hrms.install.before_install"
-after_install = "akf_hrms.install.after_install"
+# after_install = "akf_hrms.install.after_install"
 
 # Uninstallation
 # ------------
@@ -102,7 +102,7 @@ after_install = "akf_hrms.install.after_install"
 # Name of the app being installed is passed as an argument
 
 # before_app_install = "akf_hrms.utils.before_app_install"
-after_app_install = "akf_hrms.install.after_install"
+# after_app_install = "akf_hrms.install.after_install"
 
 # Integration Cleanup
 # -------------------
