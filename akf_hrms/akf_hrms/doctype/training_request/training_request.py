feb0cd300412b79f65b6c0b58b1d71d14badd86e
nabeelsocailuse <nabeel.social.use@gmail.com>
2024-05-15 13:11:08 +0000

    training request.

--- akf_hrms/akf_hrms/doctype/training_request/training_request.py
+++ akf_hrms/akf_hrms/doctype/training_request/training_request.py
@@ -1,9 +1,43 @@
-# Copyright (c) 2024, Nabeel Saleem and contributors
-# For license information, please see license.txt
-
-# import frappe
+import frappe
 from frappe.model.document import Document
 
-
 class TrainingRequest(Document):
-	pass
+    def validate(self):
+        participants_in_event = []
+        for row in self.custom_attendees:
+            query = """
+                SELECT employee 
+                FROM `tabTraining Event Employee` 
+                WHERE parent=%s 
+                AND employee=%s
+            """
+            result = frappe.db.sql(query, (self.custom_topictraining_event, row.participant_id), as_dict=True)
+        
+            if result:
+                participants_in_event.append(row.participant_id)
+        
+        if participants_in_event:
+            participants_str = ", ".join(participants_in_event)
+            frappe.throw(f"The employee {participants_str} are already in this training event.")
+        
+    def on_submit(self):
+        if self.custom_status_of_approval != 'Approved':
+            frappe.throw('Training Request can only be submitted when status is "Approved".')
+        
+        # Implementation of the existing employees 
+        event_id = self.custom_topictraining_event
+        for row in self.custom_attendees:
+            query = f"""
+                SELECT employee 
+                FROM `tabTraining Event Employee` 
+                WHERE parent='{event_id}' 
+                AND employee='{row.participant_id}'
+            """
+            result = frappe.db.sql(query, as_dict=True)
+            if not result:
+                doc = frappe.get_doc("Training Event", event_id)
+                doc.append('employees', {
+                    'employee': row.participant_id,
+                    'employee_name': row.participant_name
+                })
+                doc.save()
