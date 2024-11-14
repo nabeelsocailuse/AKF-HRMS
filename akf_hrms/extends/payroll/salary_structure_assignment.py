0e15408e5c0819251093ad9732a3b1230f66bc5e
nabeelsocialuse <nabeel.social.use@gmail.com>
2024-11-14 12:45:04 +0000

    Salary Structure Assignment - Mubarrim

--- akf_hrms/extends/payroll/salary_structure_assignment.py
+++ akf_hrms/extends/payroll/salary_structure_assignment.py
@@ -1,3 +1,4 @@
+# import frappe
 from hrms.payroll.doctype.salary_structure_assignment.salary_structure_assignment import (
     SalaryStructureAssignment
 )
@@ -6,8 +7,12 @@ class XSalaryStructureAssignment(SalaryStructureAssignment):
     def validate(self):
         super(XSalaryStructureAssignment, self).validate()
         self.calculate_hours_base()
+        self.calculate_per_day()
 
     def calculate_hours_base(self):
         # (base/30)/8
         self.custom_hourly_base = (self.base/30.0)/8.0
+
+    def calculate_per_day(self): # Mubarrim
+        self.custom_per_day = (self.base/30.0)
 
